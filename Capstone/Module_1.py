78c5d9557a3d6206075f6243aae0862bf5fac462
Ayush Agarwal <thisismemanayush@gmail.com>
2020-07-25 16:55:32 +0530

    will add notebook

--- Capstone/Module_1.py
+++
@@ -1,5 +0,0 @@
-# This Repository is totally dedicated to the Coursera Capstone Project
-# This shall not be used for any other purpose
-import pandas as pd
-import numpy as np
-print("Hello Capstone Project Course!")
