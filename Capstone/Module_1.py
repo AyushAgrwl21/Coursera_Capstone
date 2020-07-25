05cd069cc2984e6f75a31a8cd397a70f81aac78d
Ayush Agarwal <thisismemanayush@gmail.com>
2020-07-25 16:39:15 +0530

    Coursera Module 1

---
+++ Capstone/Module_1.py
@@ -0,0 +1,5 @@
+# This Repository is totally dedicated to the Coursera Capstone Project
+# This shall not be used for any other purpose
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
